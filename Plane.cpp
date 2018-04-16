eaf695e0a8f80556b4baf78db0d2eda4a079b204
Sean Rich <srich366@gmail.com>
2018-04-16 01:02:33 -0400

    Merge testing v2

---
+++ Plane.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sean on 4/16/18.
+//
+
+#include "Plane.h"
---
+++ Plane.h
@@ -0,0 +1,20 @@
+//
+// Created by sean on 4/16/18.
+//
+
+#ifndef FINALPROJECT_PLANE_H
+#define FINALPROJECT_PLANE_H
+
+#include <iostream>
+using namespace std;
+
+class Plane {
+
+public:
+    Plane(){
+        cout << "Classes work" << endl;
+    }
+};
+
+
+#endif //FINALPROJECT_PLANE_H
