576c2733d41946282ea694f2d6c662047912e8f2
Neil Ashtekar <neilcashtekar@gmail.com>
2018-04-17 19:46:48 -0400

    Adds stats file

---
+++ stats/stats/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stats
+//
+//  Created by Neil Clarke Ashtekar on 4/17/18.
+//  Copyright © 2018 Neil Clarke Ashtekar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
